93389599d205e91e9fb5292c2d288d5bbc6572c5
Nitin srivastava <nitinsri2954@gmail.com>
2023-04-09 21:41:46 +0530

    Create dequehackerRank.cpp
    Deque (STL)hackerRank solution (medium)

---
+++ dequehackerRank.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <deque>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int a[n];
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        deque<int> dq;
+        for (int i = 0; i < n; i++) {
+           
+            if (!dq.empty() && dq.front() == i - k) {
+                dq.pop_front();
+            }
+     
+            while (!dq.empty() && a[dq.back()] < a[i]) {
+                dq.pop_back();
+            }
+           
+            dq.push_back(i);
+            
+            if (i >= k - 1) {
+                cout << a[dq.front()] << " ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
